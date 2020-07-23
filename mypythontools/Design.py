56ae77493ebf991bd31b5c0069082c0201265828
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2020-07-23 17:59:45 +0900

    add list of possible method chains

--- mypythontools/Design.py
+++ mypythontools/Design.py
@@ -4,7 +4,8 @@
 __author__ = "mdonaka"
 __date__ = "2020/03/25"
 
-from typing import Type
+from collections.abc import Iterator
+from typing import Callable, Type
 
 
 class Singleton(object):
@@ -14,3 +15,37 @@ class Singleton(object):
             return cls._instance
         setattr(cls, "__init__", lambda self: None)
         return cls._instance
+
+
+class i(Iterator):
+    """
+    reference:
+    https://dawn.hateblo.jp/entry/2019/02/27/131547
+    https://gist.github.com/edvakf/90312
+    """
+
+    def __init__(self, obj) -> None:
+        self.__iter = iter(obj)
+
+    def to_list(self) -> list:
+        lst = list(self.__iter)
+        self.__iter = iter(lst)
+        return lst
+
+    def map(self, func: Callable) -> "i":
+        return i(map(func, self.__iter))
+
+    def filter(self, func: Callable) -> "i":
+        return i(filter(func, self.__iter))
+
+    def sort(self, *args, **kw) -> "i":
+        return i(sorted(list(self.__iter), *args, **kw))
+
+    def reverse(self) -> "i":
+        return i(reversed(list(self.__iter)))
+
+    def __iter__(self):
+        return self.__iter.__iter__()
+
+    def __next__(self):
+        return self.__iter.__next__()
